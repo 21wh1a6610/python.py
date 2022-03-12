d812cf7eba1852d0307fa4fc811c143e97310b03
21wh1a6610 <96616282+21wh1a6610@users.noreply.github.com>
2022-03-12 08:40:48 +0530

    Update radius of circle.py

--- radius of circle.py
+++ radius of circle.py
@@ -1,4 +1,4 @@
+def findArea(r)
 PI = 3.14
-r = float(input("enter the radius of a circle:"))
-area = PI*radius*radius
-print("area of a circle = %f" %area)
+return PI*radius*radius
+print("area of a circle = %Lf" % findArea(1.1)
